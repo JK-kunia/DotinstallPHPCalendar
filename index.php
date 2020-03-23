297cfd301bf87242f8b20afbe4d4a16ddfb14d9c
jinponpo <jk.kunia@gmail.com>
2020-03-23 23:27:34 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+require 'Calendar.php';
+
+function h($s) {
+  return htmlspecialchars($s ,ENT_QUOTES, 'UTF-8');
+}
+
+$cal = new \MyApp\Calendar();
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Calendar</title>
+</head>
+<body>
+  <table>
+    <thead>
+      <tr>
+        <th><a href="/?t=<?php echo h($cal->prev); ?>">&laquo;</a></th>
+        <th colspan="5"><?php echo h($cal->yearMonth); ?></th>
+        <th><a href="/?t=<?php echo h($cal->next); ?>">&raquo;</a></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>sun</td>
+        <td>Mon</td>
+        <td>Tue</td>
+        <td>Wed</td>
+        <td>Thu</td>
+        <td>Fri</td>
+        <td>Sat</td>
+      </tr>
+      <?php $cal->show(); ?>
+    </tbody>
+    <tfoot>
+      <tr>
+        <th colspan="7"><a href="/">Today</a></th>
+      </tr>
+    </tfoot>
+  </table>
+</body>
+</html>
